882bd29e4e740d9d4eb1aa27a9da114e28b6a18e
Felipe Dornelles <lipe.dornelles@hotmail.com>
2023-07-05 16:00:23 -0300

    feat: create users and meals routes with auth

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -1,34 +1,191 @@
 import { FastifyInstance } from "fastify";
 import { z } from "zod";
+import { checkSessionId } from "../middlewares/check-session-id";
+import { knex } from "../databse";
+import { randomUUID } from "crypto";
+import ConvertIsOnDietToBoolena from "../utils/convert-is_on_diet-to-boolean";
 
 export default async function MealsRoutes(app: FastifyInstance) {
-  app.get("/", () => {
-    return "All Meals";
-  });
+  app.get(
+    "/",
+    {
+      preHandler: [checkSessionId],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies;
 
-  app.post("/", () => {
-    return "Create Meals";
-  });
+      const meals = await knex("meals").where({ user_id: sessionId }).select();
 
-  app.get("/:id", (request) => {
-    const getParamsSchema = z.object({
-      id: z.string(),
-    });
+      return { meals: meals.map(ConvertIsOnDietToBoolena) };
+    }
+  );
 
-    const { id } = getParamsSchema.parse(request.params);
+  app.post(
+    "/",
+    {
+      preHandler: [checkSessionId],
+    },
+    async (request, replay) => {
+      const { sessionId } = request.cookies;
 
-    return `Get Meal - ${id}`;
-  });
+      const createBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        date: z.string(),
+        hour: z.string(),
+        is_on_diet: z.boolean(),
+      });
 
-  app.get("/metrics", () => {
-    return "Metrics of all Meal";
-  });
+      const { name, description, date, hour, is_on_diet } =
+        createBodySchema.parse(request.body);
 
-  app.put("/:id", () => {
-    return "Update Meal";
-  });
+      await knex("meals").insert({
+        id: randomUUID(),
+        name,
+        description,
+        date,
+        hour,
+        is_on_diet,
+        user_id: sessionId,
+      });
 
-  app.delete("/:id", () => {
-    return "Delete Meal";
-  });
+      return replay.status(201).send();
+    }
+  );
+
+  app.get(
+    "/:id",
+    {
+      preHandler: [checkSessionId],
+    },
+    async (request, replay) => {
+      const { sessionId } = request.cookies;
+
+      const getParamsSchema = z.object({
+        id: z.string(),
+      });
+
+      const { id } = getParamsSchema.parse(request.params);
+
+      const meal = await knex("meals")
+        .where({
+          id,
+          user_id: sessionId,
+        })
+        .select()
+        .first();
+
+      if (!meal) {
+        return replay.status(404).send({ message: "Meal not found" });
+      }
+
+      return { meal: ConvertIsOnDietToBoolena(meal) };
+    }
+  );
+
+  app.get(
+    "/metrics",
+    {
+      preHandler: [checkSessionId],
+    },
+    async (request) => {
+      const { sessionId } = request.cookies;
+
+      let meals = await knex("meals").where({ user_id: sessionId }).select();
+      meals = meals.map(ConvertIsOnDietToBoolena);
+
+      const on_diet = meals.filter(({ is_on_diet }) => is_on_diet === true);
+
+      return {
+        metrics: {
+          total: meals.length,
+          on_diet: on_diet.length,
+          off_diet: meals.length - on_diet.length,
+          better_sequence: "",
+        },
+      };
+    }
+  );
+
+  app.put(
+    "/:id",
+    {
+      preHandler: [checkSessionId],
+    },
+    async (request, replay) => {
+      const { sessionId } = request.cookies;
+
+      const getParamsSchema = z.object({
+        id: z.string(),
+      });
+
+      const { id } = getParamsSchema.parse(request.params);
+
+      const meal = await knex("meals")
+        .where({
+          id,
+          user_id: sessionId,
+        })
+        .select()
+        .first();
+
+      if (!meal) {
+        return replay.status(404).send({ message: "Meal not found" });
+      }
+
+      const updateBodySchema = z.object({
+        name: z.string().nullish(),
+        description: z.string().nullish(),
+        date: z.string().nullish(),
+        hour: z.string().nullish(),
+        is_on_diet: z.boolean().nullish(),
+      });
+
+      const { date, description, hour, is_on_diet, name } =
+        updateBodySchema.parse(request.body);
+
+      await knex("meals")
+        .where({
+          id,
+          user_id: sessionId,
+        })
+        .update({
+          date: date ? date : undefined,
+          description: description ? description : undefined,
+          hour: hour ? hour : undefined,
+          is_on_diet: is_on_diet ? is_on_diet : undefined,
+          name: name ? name : undefined,
+        });
+
+      return replay.status(204).send();
+    }
+  );
+
+  app.delete(
+    "/:id",
+    {
+      preHandler: [checkSessionId],
+    },
+    async (request, replay) => {
+      const { sessionId } = request.cookies;
+      const getParamsSchema = z.object({
+        id: z.string(),
+      });
+
+      const { id } = getParamsSchema.parse(request.params);
+
+      const meal = await knex("meals")
+        .where({
+          id,
+          user_id: sessionId,
+        })
+        .delete();
+
+      if (!meal) {
+        return replay.status(404).send({ message: "Meal not found" });
+      }
+
+      return replay.status(204).send();
+    }
+  );
 }
--- src/routes/users.ts
+++ src/routes/users.ts
@@ -1,11 +1,23 @@
 import { FastifyInstance } from "fastify";
+import { z } from "zod";
+import { knex } from "../databse";
+import { randomUUID } from "crypto";
 
 export default async function UsersRoute(app: FastifyInstance) {
-  app.get("/:id", () => {
-    return "Get User";
-  });
+  app.post("/", async (request, replay) => {
+    const createBodySchema = z.object({
+      name: z.string(),
+      email: z.string().email(),
+    });
 
-  app.post("/", () => {
-    return "Create User";
+    const { name, email } = createBodySchema.parse(request.body);
+
+    const resposne = await knex("users").insert({
+      id: randomUUID(),
+      email,
+      name,
+    });
+
+    return replay.status(201).send({ user: resposne });
   });
 }
